95f663541a13e7600d19de00f2b166a3c84d3e68
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-16 15:13:30 -0500

    Project init

---
+++ src/main/java/pe/com/app/AppApplication.java
@@ -0,0 +1,13 @@
+package pe.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
