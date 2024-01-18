8edef2c3f87e9b4bb44d0c0ea60d3f0b054ecfcd
Bryan Mancilla <70993543@certus.edu.pe>
2024-01-17 19:30:09 -0500

    Entidad cliente

---
+++ src/main/java/pe/com/app/model/entity/Cliente.java
@@ -0,0 +1,49 @@
+package pe.com.app.model.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable{
+
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCliente;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "correo")
+    private String correo;
+
+    @CreationTimestamp
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+}
+
+
+
+
+
+
